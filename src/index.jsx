74f31a3377e1623842443e9959ed24fecb488bae
joaoaspimienta <61059180+joaoaspimienta@users.noreply.github.com>
2021-07-05 14:03:16 -0300

    Error fix
    webpack cria um cache dentro de node_modules as vezes
    [13:58, 05/07/2021] Otavio: apaga sua node_modules
    [13:58, 05/07/2021] Otavio: apaga a pasta dist
    [13:58, 05/07/2021] Otavio: da yarn
    [13:58, 05/07/2021] Otavio: e yarn webpack
    [13:58, 05/07/2021] Otavio: e testa

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
 import { App } from './App'
 
 ReactDOM.render(<h1>Test</h1>, document.getElementById('root'))
