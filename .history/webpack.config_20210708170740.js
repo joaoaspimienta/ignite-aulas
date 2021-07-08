91a15d6d09a87542815ec6e28c123645deef124e
Joao Alejandro <joaoalejandro32@gmail.com>
2021-07-08 17:17:00 -0300

    Ambiente dev e produção

--- .history/webpack.config_20210708170740.js
+++ .history/webpack.config_20210708170740.js
@@ -5,7 +5,7 @@ const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
-  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+  devtool: 'eval-source-map',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
