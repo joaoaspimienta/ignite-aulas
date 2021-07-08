bea14bca8852a4d9a48a3464333ddb9f791474fa
joaoaspimienta <61059180+joaoaspimienta@users.noreply.github.com>
2021-07-08 09:49:22 -0300

    Estrutura do ReactJS

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App'
 
-ReactDOM.render(<h1>Test</h1>, document.getElementById('root'))
+ReactDOM.render(<App></App>, document.getElementById('root'))
